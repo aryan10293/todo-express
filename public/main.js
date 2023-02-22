34412c4ac49b8a7d72a4147ed16742e2c9a7fbdb
aondrej ryan <aondrejryan@gmail.com>
2023-02-22 00:34:29 -0800

    added some functionalilty to edit button

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 const edit = [...document.querySelectorAll('.edit')]
 edit.forEach(x => {
     x.addEventListener('click', function(){
-        console.log(this.parentNode.childNodes[1].innerText)
+        let id = this.parentNode.childNodes[1].innerText.split(' ')[0]
+        let input = document.getElementById(id)
+        console.log(input.classList.toggle('hidden'))
+        console.log(this.parentNode.childNodes[9].classList.toggle('hidden'))
     })
 })
