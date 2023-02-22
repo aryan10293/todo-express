308b8944dd74ea26ecb610743e753142881f0a23
aondrej ryan <aondrejryan@gmail.com>
2023-02-22 11:43:45 -0800

    make it better

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
