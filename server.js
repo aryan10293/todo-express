3221d49927a9e6bf18fc71fc76a694daeb8d74c6
aondrej ryan <aondrejryan11@gmail.com>
2023-02-25 04:03:52 -0800

    changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let db,
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+//app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 // app.use(helmet())
